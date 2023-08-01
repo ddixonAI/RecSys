e883b8144273f9f50dd2f83084f91a9f46434211
enDelt09 <endelt09@gmail.com>
2023-07-31 22:30:52 -0500

    some starting boilerplate

---
+++ src/model.py
@@ -0,0 +1,42 @@
+import torch
+import torch.nn as nn
+import numpy as np
+
+class Encoder(nn.Module):
+    def __init__(self, layer_num, hidden_dim, out_dim):
+        super().__init__()
+
+        self.layer_num = layer_num
+        self.hidden = nn.Linear(hidden_dim, hidden_dim)
+        self.out_layer = nn.Linear(hidden_dim, out_dim)
+        self.relu = nn.ReLU()
+    
+    def forward(self, inputs):
+        
+        x = inputs
+        for _ in range(self.layer_num):
+            x = self.hidden(x)
+            x = self.relu(x)
+        x = torch.cat([inputs, x], axis = 1)
+
+        out = self.out_layer(x)
+
+        return out
+
+class TwoTowerModel(nn.Module):
+    def __init__(self, model_config):
+        super().__init__()
+
+        self.user_encoder = Encoder()
+        self.business_encoder = Encoder()
+
+    def forward(self, user_inputs, business_inputs):
+        
+        x1 = self.user_encoder(user_inputs)
+        x2 = self.business_encoder(business_inputs)
+        out = torch.dot(x1, x2)
+
+        return out
+    
+    def init_weights(m):
+        
---
+++ src/train.py
@@ -0,0 +1,59 @@
+# Import Modules
+import os
+import time
+import torch
+from torch.utils.data import DataLoader
+import torch.nn as nn
+
+import model
+import config
+
+def train(model, loader, optimizer, loss_fn, device):
+
+    epoch_loss = 0.0
+
+    model.train()
+    for (x_user, x_bus), y in loader:
+        x_user = x_user.to(device, dtype = torch.float32)
+        x_bus = x_bus.to(device, dtype = torch.float32)
+        y = y.to(device, dtype = torch.float32)
+
+        optimizer.zero_grad()
+        y_pred = model(x_user, x_bus)
+        loss = loss_fn(y_pred, y)
+        loss.backward()
+        optimizer.step()
+        epoch_loss += loss.item()
+    
+    epoch_loss = epoch_loss / len(loader)
+
+    return epoch_loss
+
+def evaluate(model, loader, loss_fn, device):
+    
+    epoch_loss = 0.0
+
+    model.eval()
+    with torch.no_grad():
+        for (x_user, x_bus), y in loader:
+            x_user = x_user.to(device, dtype = torch.float32)
+            x_bus = x_bus.to(device, dtype = torch.float32)
+            y = y.to(device, dtype = torch.float32)
+
+            y_pred = model(x_user, x_bus)
+            loss = loss_fn(y_pred, y)
+
+            epoch_loss += loss.item()
+        
+        epoch_loss = epoch_loss / len(loader)
+
+    return epoch_loss
+    
+
+def run():
+    # Training code here
+
+    return
+
+if __name__ == "__main__":
+    run()
